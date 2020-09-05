c749a90c718b3a365823083171c7649e2792cb39
Arko <arkadiusz.kupracz@gmail.com>
2020-09-05 16:55:19 +0100

    feat: Prevent a user from adding himself to friend list

--- app/src/main/java/com/example/shared_grocery_list/activities/AddFriendActivity.kt
+++ app/src/main/java/com/example/shared_grocery_list/activities/AddFriendActivity.kt
@@ -10,6 +10,9 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_add_friend.*
 
+/**
+ * An activity for adding people to friend's list with an email
+ */
 class AddFriendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +20,10 @@ class AddFriendActivity : AppCompatActivity() {
 
         // TODO handle the case where user attempts to add himself to the list
         btn_add_friend_menu.setOnClickListener {
+            // Fetch uid for friend list reference
             val uid = intent.getStringExtra("uid")
             if (uid != null) {
+                // Read friend's email from user input
                 val friendEmail = et_grocery_item_name.text.toString()
                 if (friendEmail.isEmpty()) {
                     Toast.makeText(
@@ -26,16 +31,16 @@ class AddFriendActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT
                     ).show()
                 } else {
+                    // Fetch all users in the database and add the one that matches friend's email
                     FirebaseDatabase.getInstance().reference.child("users")
                         .addValueEventListener(object : ValueEventListener {
                             override fun onDataChange(snapshot: DataSnapshot) {
                                 for (child in snapshot.children) {
                                     val snapshotEmail = child.child("email").value
                                     val snapshotID = child.key
-                                    if (snapshotEmail == friendEmail && snapshotID != null) {
+                                    if (snapshotEmail == friendEmail && snapshotID != null && snapshotID != uid) {
                                         FirebaseDatabase.getInstance().reference.child("users")
-                                            .child(uid)
-                                            .child("friends").child(snapshotID)
+                                            .child("${uid}/friends/${snapshotID}")
                                             .setValue(friendEmail)
                                         Toast.makeText(
                                             applicationContext,
@@ -47,12 +52,10 @@ class AddFriendActivity : AppCompatActivity() {
                                 }
                             }
 
-                            override fun onCancelled(error: DatabaseError) {
-                                //TODO handle this somehow
-                            }
+                            override fun onCancelled(error: DatabaseError) {}
                         })
                 }
-            } else finish() //TODO inform user of what happened
+            } else finish()
         }
     }
 }
